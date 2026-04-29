2e0b765ec339ca730ade63bc5fa684e23da58a78
Master-Bw3 <bw1jochman@gmail.com>
2026-04-29 14:42:45 -0500

    remove confilicting and useless structure mods, add new ones

--- shell.nix
+++ shell.nix
@@ -1,8 +1,7 @@
-let
-  nixpkgsVer = "e6515129584048031d14f4ba61481a2254314018";
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/${nixpkgsVer}.tar.gz") { config = {}; overlays = []; };
-
-in pkgs.mkShell {
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShell {
   name = "shell";
 
   buildInputs = with pkgs; [
